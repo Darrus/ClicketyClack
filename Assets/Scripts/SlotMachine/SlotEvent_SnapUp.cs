04dd1b528ccd4492b8e9dc1b40ed703ba6843b91
Darrus <darrusgoh@gmail.com>
2017-10-02 11:35:42 +0800

    Added: + Holotoolkit sdk + SlotMachine.cs, Base script for slot machine to work + ISlotEvent.cs, Slot machine's event interface + SlotEvent_SnapUp.cs, Slot machine's event to snap up + 3DTextShader.shader, Shader for to cull 3D text back + 3DTextMaterial.material, Material for 3DTextShader + Anisha.font, Temporary font to test Slot Machine text + SlotMachine.prefab, Temporary prefab to test Slot Machine
    - Goh Zheng Yuan / Darrus

---
+++ Assets/Scripts/SlotMachine/SlotEvent_SnapUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotEvent_SnapUp : ISlotEvent
+{
+    public float jitterDuration = 2.0f;
+    public float jitterSpeed = 2.5f;
+    public int SnapAngle
+    {
+        get
+        {
+            return Random.Range(5, 10);
+        }
+    }
+
+    float jitterTimer;
+    Transform hand;
+
+    public SlotEvent_SnapUp(Transform _hand)
+    {
+        hand = _hand;
+        jitterTimer = jitterDuration;
+    }
+
+    public bool UpdateEvent()
+    {
+        jitterTimer -= Time.deltaTime;
+
+        if (jitterTimer > 0.0f)
+        {
+            jitterSpeed = -jitterSpeed;
+            hand.transform.Rotate(jitterSpeed, 0.0f, 0.0f);
+        }
+        else
+        {
+            jitterTimer = jitterDuration;
+            float x = hand.localEulerAngles.x;
+            hand.localEulerAngles = new Vector3(x + (float)SnapAngle, 0.0f, 0.0f);
+            return true;
+        }
+        return false;
+    }
+}
