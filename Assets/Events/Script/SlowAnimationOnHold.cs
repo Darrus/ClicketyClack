a7cea354befd1c31089c8451fe5d7cdbb77f64e2
Darrus <darrusgoh@gmail.com>
2017-10-12 09:15:05 +0800

    Reworked on Bridge event and overall how event works.
    + EventManager.cs, Currently empty, but supposedly will contain all active events in the game.
    + PickAndSnap.cs, rather then checking for mount point, it now will snap according to what the object name is.
    + TrainEventBase.cs, Base for all events, triggers a OnSolved function when an event is solved.
    
    - Goh Zheng Yuan / Darrus

---
+++ Assets/Events/Script/SlowAnimationOnHold.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+[RequireComponent(typeof(Animator))]
+public class SlowAnimationOnHold : MonoBehaviour, IManipulationHandler
+{
+    [Range(0.0f, 1.0f)]
+    public float slowedSpeed = 0.5f;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+#if DEBUG
+        Debug.Log("Animation slowed.");
+#endif
+        anim.speed = slowedSpeed;
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+#if DEBUG
+        Debug.Log("Animation speed resumed.");
+#endif
+        anim.speed = 1.0f;
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+#if DEBUG
+        Debug.Log("Animation speed resumed.");
+#endif
+        anim.speed = 1.0f;
+    }
+}
---
+++ Assets/Events/Script/TrainEventBase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TrainEventBase : MonoBehaviour
+{
+    bool solved;
+
+    public bool Solved
+    {
+        get
+        {
+            return solved;
+        }
+        set
+        {
+            solved = value;
+            if(solved)
+            {
+                OnSolved();
+            }
+        }
+    }
+
+    protected virtual void OnSolved()
+    {
+    }
+}
