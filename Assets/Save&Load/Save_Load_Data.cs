17f6cbcbaf5b765319812d2d67605bac47c57458
SIDM\150713R <love0love.ys@gmail.com>
2017-10-16 17:54:10 +0800

    ADDED Track(rail) Material In
    ~ Save_Load_Data.cs, changes will no affect game (MonoBehaviour remove)
    
    ~Shuyu

--- Assets/Save&Load/Save_Load_Data.cs
+++ Assets/Save&Load/Save_Load_Data.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Save_Load_Data : MonoBehaviour {
+public class Save_Load_Data{
 
 	// Use this for initialization
 	void Start () {
