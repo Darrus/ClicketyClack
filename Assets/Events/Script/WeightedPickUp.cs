5ca327aae6bd6dc3f7795d9c2715d8837186a5d3
Darrus <darrusgoh@gmail.com>
2017-10-17 09:39:43 +0800

    Reworked on events due to size change, removed all old prefabs.
    - Darrus / Goh Zheng Yuan

--- Assets/Events/Script/WeightedPickUp.cs
+++ Assets/Events/Script/WeightedPickUp.cs
@@ -8,7 +8,7 @@ public class WeightedPickUp : MonoBehaviour, IManipulationHandler
 {
     [Range(1.0f, 8.0f)]
     public float snapDistance = 5.0f;
-    public float speed;
+    public float force;
 
     private Rigidbody rigid;
 
@@ -36,14 +36,23 @@ public class WeightedPickUp : MonoBehaviour, IManipulationHandler
         InputManager.Instance.ClearModalInputStack();
         InputManager.Instance.PushModalInputHandler(gameObject);
         rigid.useGravity = false;
-        rigid.isKinematic = true;
+        //rigid.isKinematic = true;
     }
 
     public void OnManipulationUpdated(ManipulationEventData eventData)
     {
-        Vector3 TargetPosition = Camera.main.transform.position + Camera.main.transform.forward * snapDistance;
+        // Force = Mass * Acceleration
+        // Acceleration = Change In Velocity / Time Taken
+		Vector3 TargetPosition = Camera.main.transform.position + Camera.main.transform.forward * snapDistance;
+        float distanceSqr = (TargetPosition - transform.position).sqrMagnitude;
+        if (distanceSqr < 0.001f)
+        {
+            rigid.velocity = Vector3.zero;
+            return;
+        }
+
         Vector3 direction = (TargetPosition - transform.position).normalized;
-        Vector3 newPosition = transform.position + direction * (speed / rigid.mass);
-        transform.position = newPosition;
+        float acceleration = force / rigid.mass;
+        rigid.velocity = acceleration * direction;
     }
 }
