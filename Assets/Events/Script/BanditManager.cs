ff1484bc28fc9996d8ec1e56ac0bc72f93ebdce3
Darrus <darrusgoh@gmail.com>
2017-10-24 16:48:10 +0800

    Added human animation, and basic bandit spawning.
    + BanditManager.cs, Handles spawning of bandits around the event
    + HumanController.cs, Handles state transitions and movement of bandits
    
    - Goh Zheng Yuan / Darrus

---
+++ Assets/Events/Script/BanditManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditManager : MonoBehaviour {
+    [SerializeField]
+    EventBase eventBase;
+
+    public GameObject[] banditTypes;
+    public GameObject[] spawnPoints;
+    public float gatherRadius;
+
+    List<GameObject> activeBandits;
+    bool spawned = false;
+    bool defeat = false;
+
+    private void Awake()
+    {
+        activeBandits = new List<GameObject>();
+    }
+
+    public void SpawnBandits()
+    {
+        if (eventBase.Solved || spawned) 
+            return;
+
+        spawned = true;
+        Vector3 center = transform.position;
+
+        for(int i = 0; i < spawnPoints.Length; ++i)
+        {
+            int banditSelect = Random.Range(0, banditTypes.Length);
+            GameObject newBandit = Instantiate(banditTypes[banditSelect]);
+            newBandit.transform.position = spawnPoints[i].transform.position;
+            activeBandits.Add(newBandit);
+            HumanController control = newBandit.GetComponent<HumanController>();
+            Vector3 target = center + (spawnPoints[i].transform.position - center).normalized * gatherRadius;
+            control.RunTo(target);
+            control.ChangeState(HumanController.CharacterStates.CHEER);
+        }
+    }
+
+    public void BanditsDefeat()
+    {
+        if (!spawned)
+            return;
+
+        defeat = true;
+        foreach(GameObject bandit in activeBandits)
+        {
+            HumanController sm = bandit.GetComponent<HumanController>();
+            sm.ChangeState(HumanController.CharacterStates.DEFEAT);
+        }
+    }
+
+    private void Update()
+    {
+        if(spawned && eventBase.Solved)
+        {
+            foreach(GameObject bandit in activeBandits)
+            {
+                HumanController sm = bandit.GetComponent<HumanController>();
+                sm.ChangeState(HumanController.CharacterStates.DEFEAT);
+            }
+        }
+    }
+}
--- Assets/SceneManager/AppManager.cs
+++ Assets/SceneManager/AppManager.cs
@@ -178,5 +178,4 @@ public class AppManager : MonoBehaviour {
     {
         Temp.TheRoom.transform.DetachChildren();
     }
-
 }
