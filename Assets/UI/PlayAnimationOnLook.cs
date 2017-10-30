46abbb6bd2abf0265f5315edd1c5384342d8954b
Darrus <darrusgoh@gmail.com>
2017-10-30 09:41:16 +0800

    Added base UI assets, Recoded PlayAnimationOnLook.
    ~ PlayAnimationOnLook.cs, previously require Animations to call a Stop function, now it's no longer needed.
    
    - Goh Zheng Yuan / Darrus

--- Assets/UI/PlayAnimationOnLook.cs
+++ Assets/UI/PlayAnimationOnLook.cs
@@ -3,13 +3,17 @@
 [RequireComponent(typeof(Collider), typeof(Animator))]
 public class PlayAnimationOnLook : MonoBehaviour
 {
+    enum ANIM_STATE
+    {
+        PLAY,
+        REVERSE
+    }
+    ANIM_STATE state = ANIM_STATE.REVERSE;
     public float lookDistance;
 
     Collider col;
     Camera mainCam;
     Animator anim;
-
-    bool isLook;
 
     private void Awake()
     {
@@ -22,26 +26,35 @@ public class PlayAnimationOnLook : MonoBehaviour
     {
         RaycastHit hit;
         Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit);
-        if (hit.transform != null)
-        {
-            if (hit.distance <= lookDistance && hit.collider == col)
-            {
-                if(!isLook)
-                {
-                    isLook = true;
-                    anim.SetFloat("speedMultiplier", 1.0f);
-                }
-            }
-        }
-        else if (isLook)
-        {
-            isLook = false;
-            anim.SetFloat("speedMultiplier", -1.0f);
-        }
-    }
 
-    public void SetAnimationSpeed(float speed)
-    {
-        anim.SetFloat("speedMultiplier", speed);
+        AnimatorStateInfo animationState = anim.GetCurrentAnimatorStateInfo(0);
+        AnimatorClipInfo[] myAnimatorClip = anim.GetCurrentAnimatorClipInfo(0);
+        float myTime = myAnimatorClip[0].clip.length * animationState.normalizedTime;
+
+        switch(state)
+        {
+            case ANIM_STATE.PLAY:
+                anim.SetFloat("speedMultiplier", 1.0f);
+                if(myTime >= myAnimatorClip[0].clip.length)
+                {
+                     anim.SetFloat("speedMultiplier", 0.0f);
+                }
+                if (hit.transform == null || hit.distance >= lookDistance || hit.collider != col)
+                {
+                    state = ANIM_STATE.REVERSE;
+                }
+                break;
+            case ANIM_STATE.REVERSE:
+                anim.SetFloat("speedMultiplier", -1.0f);
+                if (myTime <= 0.0f)
+                {
+                     anim.SetFloat("speedMultiplier", 0.0f);
+                }
+                if (hit.transform != null && hit.distance <= lookDistance && hit.collider == col)
+                {
+                    state = ANIM_STATE.PLAY;
+                }
+                break;
+        }
     }
 }
