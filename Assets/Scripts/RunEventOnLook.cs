0a856cf3e6ab2fdeeb3338ebae10a49365d00edd
Darrus <darrusgoh@gmail.com>
2017-10-31 17:06:35 +0800

    Added working UI.
    + CollisionEnter.cs, Triggers unity events when collided with desired object
    + RunEventOnLook.cs, Triggers unity events when looked at
    + Pluck.cs, Script to pluck things
    + PosterSwap.cs, Swaps target poster when animation is reversed.
    + ReversePlayAnimation.cs, Controls animation forward and reverse play.
    + TicketSwap.cs, Swaps ticket material when it's back in the machine
    
    - PlayAnimationOnLook.cs, replaced with RunEventOnLook.cs
    
    - Darrus / Goh Zheng Yuan

---
+++ Assets/Scripts/RunEventOnLook.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class RunEventOnLook : MonoBehaviour
+{
+    public float lookDistance;
+    public UnityEvent onLookEvents;
+    public UnityEvent onLookAwayEvents;
+
+    Collider col;
+    Camera mainCam;
+    bool looked = false;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        col = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit);
+
+        if(!looked && hit.transform != null && hit.distance <= lookDistance && hit.collider == col)
+        {
+            looked = true;
+            onLookEvents.Invoke();
+        }
+        else if(looked && (hit.transform == null || hit.collider != col))
+        {
+            looked = false;
+            onLookAwayEvents.Invoke();
+        }
+    }
+}
---
+++ Assets/UI/Scripts/ReversePlayAnimation.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class ReversePlayAnimation : MonoBehaviour {
+
+    public enum ANIM_STATE
+    {
+        PLAYED,
+        REVERSED,
+        PLAY,
+        REVERSE
+    }
+    ANIM_STATE state = ANIM_STATE.REVERSED;
+    ANIM_STATE prevState;
+
+    public ANIM_STATE State
+    {
+        get
+        {
+            return state;
+        }
+    }
+
+    public ANIM_STATE previousState
+    {
+        get
+        {
+            return prevState;
+        }
+    }
+
+    Animator myAnimator;
+    AnimatorStateInfo animatorState;
+    AnimatorClipInfo[] animatorClip;
+    float animTime;
+
+    private void Awake()
+    {
+        prevState = state;
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animatorState = myAnimator.GetCurrentAnimatorStateInfo(0);
+        animatorClip = myAnimator.GetCurrentAnimatorClipInfo(0);
+        float animTime = animatorClip[0].clip.length * animatorState.normalizedTime;
+
+        switch (state)
+        {
+            case ANIM_STATE.PLAY:
+                myAnimator.SetFloat("speedMultiplier", 1.0f);
+                if (animTime >= animatorClip[0].clip.length)
+                {
+                    prevState = state;
+                    state = ANIM_STATE.PLAYED;
+                }
+                break;
+            case ANIM_STATE.REVERSE:
+                myAnimator.SetFloat("speedMultiplier", -1.0f);
+                if (animTime <= 0.0f)
+                {
+                    prevState = state;
+                    state = ANIM_STATE.REVERSED;
+                }
+                break;
+            default:
+                prevState = state;
+                myAnimator.SetFloat("speedMultiplier", 0.0f);
+                break;
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        prevState = state;
+        state = ANIM_STATE.PLAY;
+    }
+
+    public void ReverseAnimation()
+    {
+        prevState = state;
+        state = ANIM_STATE.REVERSE;
+    }
+
+    public void StopAnimation()
+    {
+        prevState = state;
+        state = ANIM_STATE.REVERSED;
+    }
+}
